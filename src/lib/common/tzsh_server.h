38b2c0d6658c06686e065cefc7b760667efb1ca4
Seunghun Lee <shiin.lee@samsung.com>
2015-12-07 17:19:35 +0900

    Add a header of tzsh_server_core.h, and this is included by tzsh_server.h.
    this is to hide TWS_ prefix and use TZSH_ prefix for code consistency.
    
    Change-Id: Ia11b663a7d97aa3ce4b0dd9a7534f688573dcddb

---
+++ src/lib/common/tzsh_server.h
@@ -0,0 +1,12 @@
+#ifndef _TZSH_SERVER_H_
+#define _TZSH_SERVER_H_
+
+#include <tzsh_server_core.h>
+
+typedef enum
+{
+   TZSH_SERVICE_REGION_TYPE_HANDLER = TWS_SERVICE_REGION_TYPE_HANDLER,
+   TZSH_SERVICE_REGION_TYPE_CONTENT = TWS_SERVICE_REGION_TYPE_CONTENT,
+} Tzsh_Service_Region_Type;
+
+#endif /* end of _TZSH_SERVER_H_ */
